3beb4a77d3862b7062f23214c4e9ab28609e8658
trungsliver <buiductrung.cva@gmail.com>
2025-02-16 11:52:58 +0700

    Lesson 1

---
+++ Lesson1.py
@@ -0,0 +1,30 @@
+# In / hiển thị dữ liệu ra màn hình
+print('hello world')
+print('Duc Trung')
+
+# Declare variables - Khai báo biến 
+name = 'Duc Trung'
+age = 2
+
+# 4 cách hiển thị dữ liệu ra màn hình
+    # Cách 1: Dùng dấu + 
+print('Họ tên: ' + name)
+    # Cách 2: Dùng dấu ,
+print('Tuổi:', age)
+    # Cách 3: Dùng format - truyền dữ liệu
+print(f'Tôi tên là {name}, hiện đang {age} tuổi')
+    # Cách 4: Hiển thị trên nhiều dòng
+print(f'''Minh là búp măng non
+Khiêm không biết gì cả
+Duy An không nhớ cách viết dấu ngoặc''')
+
+# Các phép toán cơ bản: 
+    # Cơ bản: + - * /
+print(1+1)
+print(7 / 2)
+    # Chia lấy dư: %
+print(7 % 2)
+    # Chia lấy nguyên: //
+print(7 // 2)
+    # Lũy thừa: **
+print(2**7)
