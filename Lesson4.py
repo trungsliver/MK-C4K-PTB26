d0dacbc01703acab752ceed2808bf7f33baa9993
trungsliver <buiductrung.cva@gmail.com>
2025-03-09 11:56:38 +0700

    Lesson 5

--- Lesson4.py
+++ Lesson4.py
@@ -56,11 +56,39 @@ print(f'Tiền điện: {cash}k VND')
 
 # ============= LUYỆN TẬP ===============
 # Câu 1: Nhập một số từ bàn phím và in ra số đó.
+number = int(input('Nhập 1 số nguyên: '))
+print('Số vừa nhập:', number)
 
 # Câu 2: Viết chương trình kiểm tra nhập vào 1 số và kiểm tra số đó là chẵn hay lẻ.
+number = int(input('Nhập 1 số nguyên: '))
+if number % 2 == 0:
+    print(number, 'là số chẵn')
+else:
+    print(number, 'là số lẻ')
 
 # Câu 3: Viết chương trình tính tổng, hiệu, tích, thương của hai số nhập từ bàn phím.
+a = 10
+b = 3
+print('Tổng 2 số:', a + b)
+print('Hiệu 2 số:', a - b)
+print('Tích 2 số:', a * b)
+print('Thương 2 số:', a / b)
 
 # Câu 4: Viết chương trình chuyển đổi từ USD sang VND (số tiền được nhập từ bàn phím).
+usd = float(input('Nhập số lượng USD muốn đổi: $'))
+vnd = usd * 25000
+print(f'${usd} = {vnd} VND')
 
 # Câu 5: Viết chương trình nhập số điểm của 3 bạn học sinh, in ra màn hình bạn có điểm thấp nhất và bạn có điểm cao nhất.
+score1 = float(input('Nhập điểm bạn thứ 1: '))
+score2 = float(input('Nhập điểm bạn thứ 2: '))
+score3 = float(input('Nhập điểm bạn thứ 3: '))
+    # Cách 1:
+if score1 >= score2 and score1 >= score3:
+    print('Bạn số 1 có điểm cao nhất')
+elif score2 >= score1 and score2 >= score3:
+    print('Bạn số 2 có điểm cao nhất')
+elif score3 >= score1 and score3 >= score2:
+    print('Bạn số 3 có điểm cao nhất')
+    # Cách 2:
+print('Điểm cao nhất là:', max(score1, score2, score3))
