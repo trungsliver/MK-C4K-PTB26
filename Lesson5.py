f666b698d2f34e2e9085a49835acd3d2b96c4f8f
trungsliver <buiductrung.cva@gmail.com>
2025-03-16 13:48:40 +0700

    Lesson 5

--- Lesson5.py
+++ Lesson5.py
@@ -2,3 +2,22 @@
 # Ví dụ: PTB26_DucTrung_CP1.py
 
 # shorturl.at/WVs6g
+
+# Trắc nghiệm
+# 1A 2B 3C 4D ....
+
+# Tự luận
+
+#               Trừ điểm        Vấn đáp
+# Bảo Hoàng:        -1          0.5/1
+# Khôi Nguyên:      -1          0.5/1
+# Gia Bách:         -1          0/2
+# Hoàng Minh:       -2          1/1
+# Duy An:                       1/2
+# Đăng Hoàng:                   1/1
+# Quốc Bảo:         
+# Đức Khiêm:        -1          1/1
+# Minh Trí:         -2          1.5/2
+# Ngọc Minh:                    0.5/1
+
+
