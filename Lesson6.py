1552f944a75e0220a3bedb0d58cf3ba581341d79
trungsliver <buiductrung.cva@gmail.com>
2025-03-23 16:08:13 +0700

    Lesson 6

--- Lesson6.py
+++ Lesson6.py
@@ -42,3 +42,27 @@ for i in range(a, b+1):
 # Bài 2: Nhập 2 số nguyên a và b từ bàn phím.
 # In ra các số nguyên trong khoảng [a, b] nếu b >= a
 # In ra các số nguyên trong khoảng [b, a] nếu a > b
+a = int(input('Nhập a: '))
+b = int(input('Nhập b: '))
+
+if b >= a:
+    print(f'Các số trong khoảng [{a}, {b}] là:')
+    for i in range(a, b+1):
+        print(i, end = ' ')
+else:
+    print(f'Các số trong khoảng [{b}, {a}] là:')
+    for i in range(b, a+1):
+        print(i, end = ' ')
+
+# Bài 3: Nhập 1 số nguyên a trong khoảng [1, 10]
+# In ra màn hình bảng cửu chương a
+a = int(input('\nNhập a: '))
+print(f'Bảng cửu chương {a}:')
+for i in range(1, 11):
+    print(f'{a} x {i} = {a * i}')
+
+# Bài 4: In ra màn hình bảng cửu chương từ 2 => 9
+for a in range(2,10):
+    print(f'\nBảng cửu chương {a}:')
+    for i in range(1, 11):
+        print(f'{a} x {i} = {a * i}')
