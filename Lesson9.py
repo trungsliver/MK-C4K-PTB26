5ef7505af3413ab38c07bbd6512c040982bb0716
trungsliver <buiductrung.cva@gmail.com>
2025-04-27 15:49:53 +0700

    Lesson 9

---
+++ Lesson9.py
@@ -0,0 +1,18 @@
+# Quy tắc đặt tên file: [Lớp]_[Hoten]_CP2.py
+# Ví dụ: PTB26_DucTrung_CP2.py
+
+# Trắc nghiệm
+# 1A 2B 3C 4D 5E .....
+# Tự luận
+
+#               Trừ điểm        Vấn đáp         Trắc nghiệm         Final
+# Bảo Hoàng:        0               1/2            4/4                8/10
+# Khôi Nguyên:      0          
+# Gia Bách:         0          
+# Hoàng Minh:      -1               0/2           4/4                 6/10
+# Duy An:           0               2/2           4/4                 9.5/10  
+# Đăng Hoàng:       0             
+# Quốc Bảo:         0
+# Đức Khiêm:        0               1/2            4/4                8.5/10
+# Minh Trí:         0          
+# Ngọc Minh:        0           
