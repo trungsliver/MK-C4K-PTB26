47ad7cac16d03eee3f9838c283fa9c42817d2edb
trungsliver <buiductrung.cva@gmail.com>
2025-05-11 11:47:22 +0700

    Lesson 10

--- Lesson10.py
+++ Lesson10.py
@@ -50,9 +50,36 @@ a.pop(2)            # Xóa bằng index (vị trí)
 
 # Luyện tập: Danh sách
     # YC1: Nhập vào từ bàn phím 1 danh sách bao gồm 10 số nguyên
+num_list = [5, 2, 9, 7, 1, 6, 0, 3, 8, 4]
     # YC2: Thêm số '-5' vào vị trí thứ 2 (index=2) trong danh sách
-    # YC3: Tính tổng các cố chẵn trong danh sách
+num_list .insert(2, -5)
+    # YC3: Tính tổng các số chẵn trong danh sách
+sum = 0
+for item in num_list:
+    if item % 2 == 0:
+        sum += item
+print('Tổng phần tử chẵn:', sum)
     # YC4: Đếm số lượng số lẻ có trong danh sách
+count = 0
+for item in num_list:
+    if item % 2 != 0:
+        count += 1
+print('Số lượng phần tử lẻ:', count)
     # YC5: Tìm giá trị phần tử lớn nhất của danh sách
+print('Giá trị phần tử lớn nhất:', max(num_list))
     # YC6: Tìm vị trí phần tử nhỏ nhất của danh sách
+min_item = min(num_list)            # Tìm giá trị phần tử nhỏ nhất
+for i in range(len(num_list)):      # Cách duyệt 1: lấy cả index và value
+    if num_list[i] == min_item:     # Kiểm tra phần tử thứ i = min_item
+        min_index = i               # Lưu vị trí index của phần tử nhỏ nhất
+print('Vị trí phần tử nhỏ nhất:', min_index)
+        # Cách ngắn gọn (bonus):
+min_index2 = num_list.index(min(num_list))
+print('Vị trí phần tử nhỏ nhất:', min_index2)
     # YC7: Dùng hàm tính giá trị trung bình của các phần tử trong danh sách
+    # Tính giá trị trung bình
+avg_value = sum(num_list) / len(num_list)
+print('Giá trị trung bình:', avg_value)     # Trước khi làm tròn
+    # Làm tròn đến chữ số thập phân thứ 2
+avg_value = round(avg_value, 2)
+print('Giá trị trung bình:', avg_value)     # Sau khi làm tròn
