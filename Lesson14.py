004ab17486c11c45cc17824424990ca3d9e90b57
trungsliver <buiductrung.cva@gmail.com>
2025-06-08 13:59:38 +0700

    Lesson 14

---
+++ Lesson14.py
@@ -0,0 +1,15 @@
+# Quy tắc đặt tên file: [Lop]_[HoTen]_hackathon.py
+# Ví dụ: PTB26_BuiDucTrung_hackathon.py
+
+# Link đề ktra: shorturl.at/8N7s4
+
+# Bảo Hoàng:            8.0  
+# Khôi Nguyên:          8.0
+# Gia Bách:             8.0
+# Hoàng Minh:           chưa ktra
+# Duy An:               9.0           
+# Đăng Hoàng:           chưa ktra        
+# Quốc Bảo:             6.5
+# Đức Khiêm:            9.5
+# Minh Trí:             8.5
+# Ngọc Minh:            8.0
